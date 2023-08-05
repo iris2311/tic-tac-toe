b8ef0aa66c03dcab0961c4f85c51f2bc2785966b
Iris Trgovec <iris_trgovec@Iriss-Air.fritz.box>
2023-08-05 13:41:46 +0200

    working version of game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string>
 #include <iostream>
+#include <algorithm>
+
+  
 #include "tic_tac_toe.h"
 
 using namespace std;
@@ -8,12 +12,53 @@ using namespace std;
 int main(void){
 
    Game game;
+   string input;
+   player current_player;
+   int current_move;
+
+   cout<<"When playing, press numbers from 1 to 9."<<endl;
+
+   cout<<"Who will play first? Press 'X' for X and 'O' for x"<<endl;
+   getline(cin,input);
+   if(input=="x")
+      input="X";
+    if(input=="o")
+        input="0";
+
+    if(input=="O")
+        current_player=O;
+    else
+        current_player=X;
 
    while(!game.game_end()){
-    
-    
-   }
 
+    cout<<"Player 1, it is your turn."<<endl;
+    cin>>current_move;
+    if(!game.valid_move(current_move)){
+        while(!game.valid_move(current_move))
+        { cout<<"Invalid move, try again."<<endl;
+          cin>>current_move;
+        }
+    }
+
+    game.update(current_player, current_move);
+    game.print_board();
+    if (game.game_end())
+        break;
+
+    cout<<"Player 2, it is your turn."<<endl;
+    current_player=game.change_player(current_player);
+    cin>>current_move;
+    if(!game.valid_move(current_move)){
+        while(!game.valid_move(current_move))
+        { cout<<"Invalid move, try again."<<endl;
+          cin>>current_move;
+        }
+    }
+    game.update(current_player, current_move);
+    game.print_board();
+    current_player=game.change_player(current_player);
+   }
 
     return 0;
 }
--- tic_tac_toe.h
+++ tic_tac_toe.h
@@ -1,10 +1,20 @@
 
-enum player{O,X};
+#include <set>
+enum player{O=0,X=1, e=2};
+
+static const char *enum_string[] = { "O", "X", "e" };
+
+std::string convert_to_enum (int tmp){
+
+    std::string MyStr(enum_string[tmp]);
+    return MyStr;
+}
 
 class Game{
 
     private:
-    int board[3][3];
+    char board[3][3];
+    std::set <int> valid_moves;
     int moves=0;
     enum player winner;
 
@@ -13,6 +23,10 @@ class Game{
     int number_of_moves();
     bool victory();
     bool game_end();
+    void print_board();
+    void update(player, int);
+    bool valid_move(int);
+    player change_player(player);
     ~Game();
 
 };
@@ -22,12 +36,21 @@ int Game::number_of_moves(){
     return moves;
 }
 
+player Game::change_player(player current){
+
+    if(current==X)
+        return O;
+    return X;
+}
+
 Game::Game(){
     
-    for (auto i=0; i<3; ++i)
-        for(auto j=0; j<3; ++j){
-            board[i][j]=0;
-        }
+    for (auto i=1; i<=9; ++i)
+        valid_moves.insert(i);
+
+    for(auto i=0; i<3; ++i)
+        for(auto j=0; j<3; ++j)
+            board[i][j]=2;
 }
 
 Game::~Game(){
@@ -37,26 +60,26 @@ Game::~Game(){
 
 bool Game:: victory() {
      for (int i = 0; i < 3; ++ i) {
-        if ((board[i][0] != 0) && (board[i][0] == board[i][1]) 
+        if ((board[i][0] != 2) && (board[i][0] == board[i][1]) 
                         && (board[i][1] == board[i][2])) {
             winner = (player)board[i][0];
             
             return true;
         }
-        if ((board[0][i] != 0) && (board[0][i] == board[1][i]) 
+        if ((board[0][i] != 2) && (board[0][i] == board[1][i]) 
                         && (board[1][i] == board[2][i])) {
             winner = (player)board[0][i];
            
             return true;
         }            
     }
-    if ((board[1][1] != 0) && (board[0][0] == board[1][1]) 
+    if ((board[1][1] != 2) && (board[0][0] == board[1][1]) 
                         && (board[1][1] == board[2][2])) {
         winner = (player)board[1][1];
        
         return true;
     }
-    if ((board[1][1] != 0) && (board[0][2] == board[1][1]) 
+    if ((board[1][1] != 2) && (board[0][2] == board[1][1]) 
                         && (board[1][1] == board[2][0])) {
         winner= (player)board[1][1];
         return true;
@@ -73,4 +96,60 @@ bool Game::game_end(){
     return false;
     
     
+}
+
+void Game::print_board(){
+
+    for (auto i=0; i<3; ++i){
+        for(auto j=0; j<3; ++j){
+            if(board[i][j]==2)
+                std::cout<<" - "<<" ";
+            else
+                 std::cout<<convert_to_enum(board[i][j])<<"  ";
+        }
+
+        std::cout<<std::endl;
+    }
+
+}
+
+void Game::update(player current_player, int move){
+
+    int row, column;
+
+    switch(move){
+        case 1: row=0; column=0; 
+            break;
+        case 2: row=0; column=1; 
+            break;
+        case 3: row=0; column=2; 
+            break;
+        case 4: row=1; column=0;
+            break;
+        case 5: row=1; column=1; 
+            break;
+        case 6: row=1; column=2; 
+            break;
+        case 7: row=2; column=0; 
+            break;
+        case 8: row=2; column=1; 
+            break;
+        case 9: row=2; column=2; 
+            break;
+    }
+
+    board[row][column]=player(current_player);
+    valid_moves.erase(move);
+    moves++;
+
+}
+
+bool Game::valid_move(int move){
+
+  if(valid_moves.find(move)==valid_moves.end())
+    return false;
+
+  return true;
+    
+
 }
